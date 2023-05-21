441014bb4e3590ebb6c6fbfe29db89a6f1417d0a
Om Mahale <omanohar15@gmail.com>
2023-05-22 02:19:35 +0530

    ✅compose finish

--- Serve_Chat/settings.py
+++ Serve_Chat/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Serve_Chat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("PSQL_DB"),
+        'USER': os.getenv("PSQL_USER"),
+        'PASSWORD': os.getenv("PSQL_PASSWORD"),
+        'HOST': os.getenv("PSQL_HOST"),
     }
 }
 
