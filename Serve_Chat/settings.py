6046a3e96c55a8df325b343e2339b8f9896bf30a
Om Mahale <omanohar15@gmail.com>
2023-05-21 23:56:10 +0530

    ⚙️Config

--- Serve_Chat/settings.py
+++ Serve_Chat/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uq_4of_lv4t1j7mg!i4-k#@&bbwfc7&b=0^^sp3=bx(%t05bba'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
