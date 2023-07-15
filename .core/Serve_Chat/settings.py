86f83c87f80890ea64795d8d0ce71bda6579cd02
Om Mahale <omanohar15@gmail.com>
2023-07-15 19:14:45 +0530

    ✅Auth ORM complete

--- .core/Serve_Chat/settings.py
+++ .core/Serve_Chat/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user',
+    'Serve_Chat.auth'
 ]
 
 MIDDLEWARE = [
--- .core/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.views import APIView
-# Create your views here.
-
-
-class Index(APIView):
-    def get(self,request):
-        return Response({"message":"This is users app"})
